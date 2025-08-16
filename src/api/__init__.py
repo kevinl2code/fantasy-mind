e95aa681628162b8f49f47eb4eaed19fc82a19e5
Kevin <ksmith35@wgu.edu>
2025-08-16 18:00:22 -0500

    chore: updated another import with src

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,5 +1,5 @@
 # from api.routes.auth import router as auth_router
-from api.routes.train import router as train_router  # Add this line
+from src.api.routes.train import router as train_router  # Add this line
 
 __all__ = [
     # "auth_router",
